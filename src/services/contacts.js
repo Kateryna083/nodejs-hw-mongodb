57e031432ab51fbd6ec0b2ca0d1f0f930f71ac63
Kateryna083 <159190252+Kateryna083@users.noreply.github.com>
2024-10-25 16:09:30 +0300

    test-4

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,15 +1,15 @@
 import ContactCollection from '../db/models/Contact.js';
 
-// export const getContacts = () => ContactCollection.find();
+export const getContacts = () => ContactCollection.find();
 
-// export const getContactById = (id) => ContactCollection.findById(id);
+export const getContactById = (id) => ContactCollection.findById(id);
 
-export const getContacts = async () => {
-  const contacts = await ContactCollection.find();
-  return contacts;
-};
+// export const getContacts = async () => {
+//   const contacts = await ContactCollection.find();
+//   return contacts;
+// };
 
-export const getContactById = async (id) => {
-  const contact = await ContactCollection.findById(id);
-  return contact;
-};
+// export const getContactById = async (id) => {
+//   const contact = await ContactCollection.findById(id);
+//   return contact;
+// };
