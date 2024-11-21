70c55677007d8c0fe9506d1eeed3b9d19fa5830c
Kateryna083 <159190252+Kateryna083@users.noreply.github.com>
2024-11-21 12:34:08 +0200

    added-password-reset

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import * as authControllers from '../controllers/auth.js';
+import { requestResetEmailController } from '../controllers/auth.js';
 
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 import validateBody from '../utils/validateBody.js';
 
 import { authRegisterSchema, authLoginSchema } from '../validation/auth.js';
+import { requestResetEmailSchema } from '../validation/auth.js';
 
 const authRouter = Router();
 
@@ -26,5 +28,11 @@ authRouter.post(
 );
 
 authRouter.post('/logout', ctrlWrapper(authControllers.logoutController));
+
+authRouter.post(
+  '/request-reset-email',
+  validateBody(requestResetEmailSchema),
+  ctrlWrapper(requestResetEmailController),
+);
 
 export default authRouter;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,7 @@
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 import { randomBytes } from 'crypto';
+import jwt from 'jsonwebtoken';
 
 import UserCollection from '../db/models/User.js';
 import SessionCollection from '../db/models/Session.js';
@@ -9,6 +10,10 @@ import {
   accessTokenLifetime,
   refreshTokenLifetime,
 } from '../constants/users.js';
+import { SMTP } from '../constants/index.js';
+
+import { env } from '../utils/env.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 const createSession = () => {
   const accessToken = randomBytes(30).toString('base64');
@@ -83,3 +88,27 @@ export const logout = (sessionId) =>
 export const findSession = (filter) => SessionCollection.findOne(filter);
 
 export const findUser = (filter) => UserCollection.findOne(filter);
+
+export const requestResetToken = async (email) => {
+  const user = await UserCollection.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    env('JWT_SECRET'),
+    {
+      expiresIn: '15m',
+    },
+  );
+
+  await sendEmail({
+    from: env(SMTP.SMTP_FROM),
+    to: email,
+    subject: 'Reset your password',
+    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+  });
+};
